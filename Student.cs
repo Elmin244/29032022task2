72b74ef7835747b27e9247ef27468de6bf98874c
unknown <tu7d5w0m5@code.edu.az>
2022-03-29 00:38:48 -0800

    first commit

---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _29032022task2
+{
+    internal class Student
+    {
+        public Student(string fullName,int age,)
+        {
+            GroupNo = _groupno;
+            FullName = fullName;
+            Age = age;
+        }
+        private int _groupno;
+        public string FullName { get; set; }
+        public int GroupNo { get => this._groupno;
+            set
+            {
+                if (CheckGroupNo(value))
+                {
+                    _groupno = value;
+                }
+            }
+        public int Age{ get; set; }
+        public bool CheckGroupNo(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text) && char.IsUpper(text[0])))
+            {
+                for (int i= 1; i <text.Length; i++)
+                {
+                    char.IsDigit(text[i]);
+                }
+                return true;
+            }
+            return false;
+        }
+        
+    }
+}
